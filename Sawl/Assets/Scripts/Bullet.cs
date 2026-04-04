4f5b5cea19997587ffead62d4d7d0e1c0c261c5b
E-Dawg67 <eht24a@acu.edu>
2026-04-04 17:01:06 -0500

    Fixed Bullets and Implemented Enemy Spawning / Waves

--- Sawl/Assets/Scripts/Bullet.cs
+++ Sawl/Assets/Scripts/Bullet.cs
@@ -3,33 +3,19 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public GameObject textOBJ;
-    void Update()
+    Rigidbody rb;
+    void Start()
     {
-        transform.position += transform.forward * 25f * Time.deltaTime;
+        rb = GetComponent<Rigidbody>();
+        rb.linearVelocity = transform.forward * 25f;
     }
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if(collision.gameObject.tag == "Enemy")
         {
             Destroy(collision.gameObject);
-            /*
-            TMPro.TextMeshProUGUI enemytext = GameObject.Find("EnemyCount").GetComponent<TMPro.TextMeshProUGUI>();
-            GameObject.Find("GameManager").GetComponent<managerScript>().enemycount--;
-            enemytext.text = GameObject.Find("GameManager").GetComponent<managerScript>().enemycount.ToString() + " Enemies Left";
-            GameObject text = Instantiate(textOBJ, new Vector3(transform.position.x, transform.position.y, transform.position.z - 1f), textOBJ.transform.rotation);
-            text.GetComponent<TextMesh>().text = "Hit";
-            text.transform.localScale *= 5f;
-            Destroy(text, 2f);*/
+            GameObject.Find("GameManager").GetComponent<managerScript>().eliminateEnemy();
         }
-        else
-        {
-            /*S
-            GameObject text = Instantiate(textOBJ, new Vector3(transform.position.x, transform.position.y, transform.position.z - 1f),textOBJ.transform.rotation);
-            text.GetComponent<TextMesh>().text = "Miss";
-            Destroy(text, 2f);
-            */
-        }
-            Destroy(gameObject);
+        Destroy(gameObject);
     }
 }
--- Sawl/Assets/Scripts/managerScript.cs
+++ Sawl/Assets/Scripts/managerScript.cs
@@ -4,12 +4,40 @@ public class managerScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject Enemy;
-    public int enemycount = 5;
-    
+    public int enemycount, wave = 1;
+    public GameObject[] spawns = new GameObject[14];
+    private int[] waveSize = { 5, 6, 8, 11, 13, 14 };
     void Start()
     {
         Cursor.lockState = CursorLockMode.Confined;
-
+        //display wave number
+        Invoke("spawnWave", 4f);
+    }
+    void spawnWave()
+    {
+        //get rid of wave number ui
+        enemycount = waveSize[wave - 1];
+        for (int i = 0; i < waveSize[wave - 1];  i++)
+        {
+            Instantiate(Enemy, spawns[i].transform.position, Quaternion.identity);
+        }
+    }
+    public void eliminateEnemy()
+    {
+        enemycount--;
+        if (enemycount == 0)
+        {
+            wave++;
+            if (wave > 5)
+            {
+                //Display Victory Screen!
+            }
+            else
+            {
+                //display wave
+                Invoke("spawnWave", 4f);
+            }
+        }
     }
     
 }
